7beeb37e9d93a71f25fb493d5c80dbee7210f08b
akshith99 <ak38293@gmail.com>
2022-11-10 23:04:27 +0530

    Initial Commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,55 @@
-import React, {useState, useEffect} from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import './App.css'
+import { requestRobots, setSearchField } from "../actions";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 
-function App() {
 
-    const [robots, setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    const [count,setCount] = useState(0)
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(users => {setRobots(users)});
-        console.log(count)
-    },[count])
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
+class App extends Component {
+    
 
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
+    componentDidMount() {
+        this.props.onRequestRobots();
     }
 
-        const filteredRobots = robots.filter(robot=>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    render() {
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        return !robots.length ?
+        return isPending?
             <h1 className="tc f2">Loading</h1>
             : (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <button onClick={()=>setCount(count+1)}>Click Me!</button>
-                    <SearchBox searchChange={onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange} />
                     <Scroll>
                         <ErrorBoundary>
-                        <CardList robots={filteredRobots}/>
+                            <CardList robots={filteredRobots} />
                         </ErrorBoundary>
                     </Scroll>
-                    
+
                 </div>
             );
     }
+}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
